f36d75b9010c54e80998de8482e6c9916206d1f1
emericdefay <emericdefay@gmail.com>
2023-02-22 20:22:39 +0100

    doc(tools): docstring

--- app/tools/repair_upt.py
+++ app/tools/repair_upt.py
@@ -5,6 +5,14 @@ from updater.paths import UPT_PATH
 
 
 def repair_update_json() -> None: 
+    """
+    Writes default settings to the update configuration file. This is used when
+    the update configuration file is missing or corrupted, to ensure that the
+    application can operate with default settings.
+
+    Returns:
+        None
+    """
     with open(f'{UPT_PATH}', 'w') as f:
         json.dump(DEF_STGS, f)
 
